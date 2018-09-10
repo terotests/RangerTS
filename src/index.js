dc42375977831642a59bdc21c7183f4bf70d6e53
Tero Tolonen <tero@sumpli.com>
2018-09-10 09:17:26 +0300

    removed not used files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// the starting point of the applictaion rangerts
-exports.__esModule = true;
-function HelloWorld() {
-    return 'Hello World!';
-}
-exports.HelloWorld = HelloWorld;
